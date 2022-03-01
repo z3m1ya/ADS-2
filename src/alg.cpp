2a365277a6491d86c86dd89d1c6350e91f40e748
z3m1ya <96194885+z3m1ya@users.noreply.github.com>
2022-03-01 10:22:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
+#include <iostream>
 
 double pown(double value, uint16_t n) {
     double temp = 1;
@@ -21,7 +21,7 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    if (count == 0) 
+    if (count == 0)
         return 1;
     else
         return calcItem(x, count) + expn(x, count - 1);
@@ -31,12 +31,14 @@ double sinn(double x, uint16_t count) {
     if (count == 0)
         return 0;
     else
-        return pown(-1, count - 1) * calcItem(x, 2 * count - 1) + sinn(x, count - 1);
+        return pown(-1, count - 1) 
+        * calcItem(x, 2 * count - 1) + sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
     if (count == 0)
         return 0;
     else
-        return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
+        return pown(-1, count - 1) 
+        * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
 }
